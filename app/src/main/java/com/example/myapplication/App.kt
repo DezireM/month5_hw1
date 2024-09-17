7b28a66ea67c118ec4e0a03ec3ebbdbbdbb2ea8a
DezireM <deziremaatkabylova@gmail.com>
2024-09-17 17:06:22 +0600

    changed color set

---
+++ app/src/main/java/com/example/myapplication/App.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+import android.app.Application
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object App: Application() {
+
+    val api: LoveApiService = Retrofit.Builder()
+        .baseUrl("https://love-calculator.p.rapidapi.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build().create(LoveApiService::class.java)
+
+
+}
