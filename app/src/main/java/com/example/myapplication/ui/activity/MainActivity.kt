663b5aba3911d32ea82ea005cb7b2a3d3c98d673
DezireM <deziremaatkabylova@gmail.com>
2024-09-17 18:05:59 +0600

    changed color set

--- app/src/main/java/com/example/myapplication/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/activity/MainActivity.kt
@@ -1,28 +1,45 @@
 package com.example.myapplication.ui.activity
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.LoveContract
 import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityMainBinding
 import com.example.myapplication.ui.fragments.LoveCalculatorFragment
 
-class MainActivity : AppCompatActivity(), LoveContract {
+class MainActivity : AppCompatActivity() {
+
     private val binding by lazy {
-        ActivityMainBinding.inflate((layoutInflater))
+        ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        if (savedInstanceState != null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, LoveCalculatorFragment())
-                .commit()
+        viewModel.count.observe(this) { count ->
+            binding.tvCount.text = count.toString()
         }
 
+        viewModel.toastMessage.observe(this) { message ->
+            message?.let {
+                Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+            }
+        }
+        viewModel.textColor.observe(this) { color ->
+            binding.tvCount.setTextColor(color)
+        }
+        binding.apply {
+            btnIncrement.setOnClickListener {
+                viewModel.increment()
+            }
+            btnDecrement.setOnClickListener {
+                viewModel.decrement()
+            }
+        }
     }
-
-
 }
