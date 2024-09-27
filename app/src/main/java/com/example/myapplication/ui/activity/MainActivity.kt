64b5d0d5b80ff5abd93f0969676a559e2297ecee
DezireM <deziremaatkabylova@gmail.com>
2024-09-27 18:40:56 +0600

    Resolved merge conflicts in MainActivity.kt

--- app/src/main/java/com/example/myapplication/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/activity/MainActivity.kt
@@ -2,27 +2,27 @@ package com.example.myapplication.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import com.example.myapplication.LoveContract
 import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityMainBinding
 import com.example.myapplication.ui.fragments.LoveCalculatorFragment
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity(), LoveContract {
-    private val binding by lazy {
-        ActivityMainBinding.inflate((layoutInflater))
-    }
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater) // Binding setup
         setContentView(binding.root)
 
-        if (savedInstanceState != null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, LoveCalculatorFragment())
-                .commit()
-        }
-
+        setupNavigation() // Navigation setup method
     }
 
-
+    private fun setupNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        navController.setGraph(R.navigation.nav_graph) // Navigation logic
+    }
 }
